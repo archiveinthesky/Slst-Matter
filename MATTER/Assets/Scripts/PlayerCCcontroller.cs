ca0ca6ac4ce610f1ccd6b6bc958db5982e13ec93
DavH-RSES <rses.davidh@gmail.com>
2019-11-06 16:24:54 +0800

    V.1_Project Update
    basic FPS system installed
    npc CC system

---
+++ MATTER/Assets/Scripts/PlayerCCcontroller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerCCcontroller : MonoBehaviour 
+{
+	public string cc = "";
+	public bool preped = false;
+	public Canvas CANVAS;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (preped)
+		{
+			printCC();
+		}
+	}
+
+	void printCC()
+	{
+		// preped = false;
+		Text TEXT = CANVAS.GetComponentInChildren<Text>();
+		TEXT.GetComponent<Text>().text = cc;
+	}
+}
