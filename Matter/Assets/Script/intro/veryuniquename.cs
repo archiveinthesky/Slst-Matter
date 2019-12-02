3af71141511a8a6c6c209cffde2e061cb6221c72
ak <andrew.mutien@gmail.com>
2019-12-02 23:06:44 +0800

    creating basic ui sample

---
+++ Matter/Assets/Script/intro/veryuniquename.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class veryuniquename : MonoBehaviour
+{
+
+    public void Hellothere()
+    {
+        Debug.Log("Hello");
+    }
+
+}
+
