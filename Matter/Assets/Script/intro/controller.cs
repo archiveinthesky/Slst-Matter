211e17ee1f1fd21c575cb8c19150f2998dd02c89
ak <andrew.mutien@gmail.com>
2019-12-07 00:23:46 +0800

    created basic menu animsys
    Needs to be changed to click compatible

--- Matter/Assets/Script/intro/controller.cs
+++ Matter/Assets/Script/intro/controller.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class controller : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject fadeob1, slothtext;
+    public GameObject fadeob1, slothtext, blayer;
     void Start()
     {
         StartCoroutine(slothlogo());
+        //StartCoroutine(mainmenuin());
     }
 
     // Update is called once per frame
@@ -20,11 +21,17 @@ public class controller : MonoBehaviour
     IEnumerator slothlogo()
     {
         gameObject.GetComponent<fadeIn>().fadell(fadeob1);
-        iTween.MoveTo(slothtext, iTween.Hash("x", 580, "time", 1, "delay", 1.2f, "easetype", "Math.easeOutCirc"));
-        iTween.MoveTo(fadeob1, iTween.Hash("x", 270, "time", 1, "delay", 1.2f, "easetype", "Math.easeOutCirc"));
+        iTween.MoveTo(slothtext, iTween.Hash("x", 400, "time", 1, "delay", 1.2f, "easetype", "easeOutCirc"));
+        iTween.MoveTo(blayer, iTween.Hash("x", -800, "time", 1, "delay", 1.2f, "easetype", "easeOutCirc"));
+        iTween.MoveTo(fadeob1, iTween.Hash("x", -253, "time", 1, "delay", 1.2f, "easetype", "easeOutCirc"));
         yield return new WaitForSeconds(3);
-        gameObject.GetComponent<fadeout>().fadeoutpub(fadeob1);
-        yield return new WaitForSeconds(1.5f);
-        gameObject.GetComponent<fadeout>().fadeoutpub(slothtext);   
+        gameObject.GetComponent<fadeout>().fadeoutsg(blayer);
+        yield return new WaitForSeconds(2);
+        gameObject.GetComponent<fadeout>().fadeoutpub(slothtext, fadeob1);
+
     }
+    
+
+
+    
 }
--- Matter/Assets/Script/intro/fadeIn.cs
+++ Matter/Assets/Script/intro/fadeIn.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class fadeIn : MonoBehaviour
 {
-    SpriteRenderer rend;
+    SpriteRenderer rendS;
+    SpriteRenderer rendS1, rendS2;
+    Image rendI;
+    Image rendI1, rendI2;
     Color c;
+    Color c1, c2;
 
+    
+    char rendStat;
+
+    int r1stat, r2stat;
     // Start is called before the first frame update
 
     void Start()
@@ -16,10 +25,54 @@ public class fadeIn : MonoBehaviour
     public void fadell(GameObject tempfadeobj)
     {   
 
-        rend = tempfadeobj.GetComponent<SpriteRenderer>();
-        c = rend.material.color;
+        rendS = tempfadeobj.GetComponent<SpriteRenderer>();
+        if (rendS == null)
+        {
+            rendI = tempfadeobj.GetComponent<Image>();
+            c = rendI.material.color;
+            rendStat = 'i';
+        }
+        else 
+        {
+            c = rendS.material.color;
+            rendStat = 's';
+        }
+        
         c.a = 0;
         StartCoroutine(fade());
+    }
+
+    public void fadedb(GameObject tmpfadeob1, GameObject tmpfadeob2)
+    {
+        rendS1 = tmpfadeob1.GetComponent<SpriteRenderer>();
+        if (rendS1 == null)
+        {
+            rendI1 = tmpfadeob1.GetComponent<Image>();
+            c1 = rendI1.material.color;
+            r1stat = 0;
+        }
+        else 
+        {
+            c1 = rendS1.material.color;
+            r1stat = 1;
+        }
+
+        rendS2 = tmpfadeob2.GetComponent<SpriteRenderer>();
+        if (rendS2 == null)
+        {
+            rendI2 = tmpfadeob2.GetComponent<Image>();
+            c2 = rendI2.material.color;
+            r2stat = 0;
+        }
+        else 
+        {
+            c2 = rendS2.material.color;
+            r2stat = 1;
+        }
+        
+        c1.a = 0;
+        c2.a = 0;
+        StartCoroutine(fadedouble());
     }
 
     IEnumerator fade()
@@ -27,11 +80,55 @@ public class fadeIn : MonoBehaviour
         
         for (float f = 0F ; f <= 1f; f += 0.05f)
         {
-            c = rend.material.color;
-            c.a = f;
-            rend.material.color = c;
+            if (rendStat == 'i')
+            {
+                c = rendI.material.color;
+                c.a = f;
+                rendI.material.color = c;
+            }
+            else if (rendStat == 's')
+            {
+                c = rendS.material.color;
+                c.a = f;
+                rendS.material.color = c;
+            }            
             yield return new WaitForSeconds(0.05f);
         }
-        c.a = 0f;
+        
+    }
+
+    IEnumerator fadedouble()
+    {
+        for (float f = 0F ; f <= 1f; f += 0.05f)
+        {
+            if (r1stat == 0)
+            {
+                c1 = rendI1.material.color;
+                c1.a = f;
+                rendI1.material.color = c1;
+            }
+            else if (r1stat == 1)
+            {
+                c1 = rendS1.material.color;
+                c1.a = f;
+                rendS1.material.color = c1;
+            }   
+
+            if (r2stat == 0)
+            {
+                c2 = rendI2.material.color;
+                c2.a = f;
+                rendI2.material.color = c2;
+            }
+            else if (r2stat == 1)
+            {
+                c2 = rendS2.material.color;
+                c2.a = f;
+                rendS2.material.color = c2;
+            }        
+            yield return new WaitForSeconds(0.05f);
+            Debug.Log(c1.a);
+            Debug.Log(c2.a);
+        }
     }
 }
