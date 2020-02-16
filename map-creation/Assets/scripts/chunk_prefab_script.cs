373700313da0f149b89be0dbd567c1d497dd6bc8
ultddeveloper <andrew.mutien@gmail.com>
2020-02-16 22:50:03 +0800

    finished map

---
+++ map-creation/Assets/scripts/chunk_prefab_script.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chunk_prefab_script : MonoBehaviour
+{
+    public List<Sprite> prefabs = new List<Sprite>();
+    public int chunk_index;
+    void Start()
+    {
+        GetComponentInParent<SpriteRenderer>().sprite = prefabs[chunk_index - 1];
+    }
+}
