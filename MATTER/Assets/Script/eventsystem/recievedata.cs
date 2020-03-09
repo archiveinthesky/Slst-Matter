eaf555dba3e66cd3c6d015f6c7383d3c801b8a9d
ultddeveloper <andrew.mutien@gmail.com>
2020-03-09 23:13:14 +0800

    Completed Text part of new comers welcome

--- MATTER/Assets/Script/eventsystem/recievedata.cs
+++ MATTER/Assets/Script/eventsystem/recievedata.cs
@@ -18,6 +18,157 @@ public class recievedata : MonoBehaviour
 
     void applyEffect(string changes)
     {
+        char p = '\u0070';
+        char a = '\u0061';
+        char o = '\u006f';
+        char u = '\u0075';
+        char h = '\u0068';
+        char sep = '\u002c';
+        char add = '\u002b';
+        char min = '\u002d';
 
+        for (int i = 0; i < changes.Length; i++)
+        {
+            if (changes[i] == p)
+            {
+                i++;
+                if (changes[i] == add)
+                {
+                    i++;
+                    int editval = 0;
+                    while (changes[i] != sep)
+                    {
+                        editval *= 10;
+                        editval += int.Parse(changes[i].ToString());
+                    }
+                    //hp + value
+                    i++;
+                }
+                if (changes[i] == min)
+                {
+                    i++;
+                    int editval = 0;
+                    while (changes[i] != sep)
+                    {
+                        editval *= 10;
+                        editval += int.Parse(changes[i].ToString());
+                    }
+                    //hp - value
+                    i++;
+                }
+            }
+            else if (changes[i] == a)
+            {
+                i++;
+                if (changes[i] == add)
+                {
+                    i++;
+                    int editval = 0;
+                    while (changes[i] != sep)
+                    {
+                        editval *= 10;
+                        editval += int.Parse(changes[i].ToString());
+                    }
+                    //waterStorage + value
+                    i++;
+                }
+                if (changes[i] == min)
+                {
+                    i++;
+                    int editval = 0;
+                    while (changes[i] != sep)
+                    {
+                        editval *= 10;
+                        editval += int.Parse(changes[i].ToString());
+                    }
+                    //waterStorage - value
+                    i++;
+                }
+            }
+            else if (changes[i] == o)
+            {
+                i++;
+                if (changes[i] == add)
+                {
+                    i++;
+                    int editval = 0;
+                    while (changes[i] != sep)
+                    {
+                        editval *= 10;
+                        editval += int.Parse(changes[i].ToString());
+                    }
+                    //foodStorage + value
+                    i++;
+                }
+                if (changes[i] == min)
+                {
+                    i++;
+                    int editval = 0;
+                    while (changes[i] != sep)
+                    {
+                        editval *= 10;
+                        editval += int.Parse(changes[i].ToString());
+                    }
+                    //foodStorage - value
+                    i++;
+                }
+            }
+            else if (changes[i] == u)
+            {
+                i++;
+                if (changes[i] == add)
+                {
+                    i++;
+                    int editval = 0;
+                    while (changes[i] != sep)
+                    {
+                        editval *= 10;
+                        editval += int.Parse(changes[i].ToString());
+                    }
+                    //hunger + value
+                    i++;
+                }
+                if (changes[i] == min)
+                {
+                    i++;
+                    int editval = 0;
+                    while (changes[i] != sep)
+                    {
+                        editval *= 10;
+                        editval += int.Parse(changes[i].ToString());
+                    }
+                    //hunger - value
+                    i++;
+                }
+            }
+            else if (changes[i] == h)
+            {
+                i++;
+                if (changes[i] == add)
+                {
+                    i++;
+                    int editval = 0;
+                    while (changes[i] != sep)
+                    {
+                        editval *= 10;
+                        editval += int.Parse(changes[i].ToString());
+                    }
+                    //thirst + value
+                    i++;
+                }
+                if (changes[i] == min)
+                {
+                    i++;
+                    int editval = 0;
+                    while (changes[i] != sep)
+                    {
+                        editval *= 10;
+                        editval += int.Parse(changes[i].ToString());
+                    }
+                    //thurst - value
+                    i++;
+                }
+            }
+        }
     }
 }
--- MATTER/Assets/Script/newPlayers/showscript.cs
+++ MATTER/Assets/Script/newPlayers/showscript.cs
@@ -5,28 +5,62 @@ using UnityEngine.UI;
 public class showscript : MonoBehaviour
 {
     public GameObject dpti, dpsub, block;
-    public Animator dptia, dpsuba;
+    public Animator dptia, dpsuba, blocka;
     public List<string> title, subti, anim;
-    public List<int> animSp;
-    Color fade;
+    public List<float> animSp;
+
 
     void Start()
     {
-        title.Add("Sloth Studios");
-        subti.Add("Presents");
+        title.Add("樹懶工作室");
+        subti.Add("為您呈現");
         anim.Add("fade");
-        animSp.Add(1);
+        animSp.Add(2f);
 
-        title.Add("Chingshin Academy");
-        subti.Add("In association with");
+        title.Add("臺北市私立靜心高級中學");
+        subti.Add("指導");
         anim.Add("fade");
-        animSp.Add(1);
+        animSp.Add(2f);
 
-        title.Add("Project: Matter");
+        title.Add("Matter: 末日生存");
         subti.Add("");
         anim.Add("fade");
-        animSp.Add(4);
+        animSp.Add(4f);
 
+        title.Add("西元2025年，世界上最大的藥廠，創世公司");
+        subti.Add("");
+        anim.Add("type");
+        animSp.Add(0.1f);
+
+        title.Add("研發出了癌症的對抗藥物");
+        subti.Add("");
+        anim.Add("type");
+        animSp.Add(0.1f);
+
+        title.Add("你身為開發團隊的首席科學家");
+        subti.Add("");
+        anim.Add("type");
+        animSp.Add(0.1f);
+
+        title.Add("獲得了媒體大量的關注");
+        subti.Add("");
+        anim.Add("type");
+        animSp.Add(0.1f);
+
+        title.Add("然而，三個月後，服用藥物的病患卻集體昏迷");
+        subti.Add("");
+        anim.Add("type");
+        animSp.Add(0.1f);
+
+        title.Add("為了應付這個突發狀況");
+        subti.Add("");
+        anim.Add("type");
+        animSp.Add(0.1f);
+
+        title.Add("公司急忙將你送回提煉解藥的植物的叢林");
+        subti.Add("");
+        anim.Add("type");
+        animSp.Add(0.1f);
         StartCoroutine(sequence());
     }
     void Update()
@@ -37,29 +71,56 @@ public class showscript : MonoBehaviour
     IEnumerator sequence()
     {
         //yield return new WaitForSeconds(1);
-        block.SetActive(false);
+        block.SetActive(true);
 
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < title.Count; i++)
         {
             dpti.GetComponent<Text>().text = title[i];
             dpsub.GetComponent<Text>().text = subti[i];
-            if (anim[0] == "fade")
+            if (anim[i] == "fade")
             {
                 StartCoroutine(fadetxt(animSp[i]));
+                yield return new WaitForSeconds(2 + animSp[i]);
             }
-            yield return new WaitForSeconds(2 + animSp[i]);
+            else if (anim[i] == "type")
+            {
+                StartCoroutine(typetxt(animSp[i], title[i], subti[i]));
+                yield return new WaitForSeconds(animSp[i] * (title[i].Length + subti[i].Length) + 2);
+            }
+
         }
+
+        dpsub.SetActive(false);
+        dpti.SetActive(false);
+
     }
 
 
-    IEnumerator fadetxt(int waitTime)
+    IEnumerator fadetxt(float waitTime)
     {
-        dptia.Play("suf");
-        dpsuba.Play("sufo");
+        block.SetActive(true);
+        blocka.Play("block-fade-out");
         yield return new WaitForSeconds(1);
         yield return new WaitForSeconds(waitTime);
-        dptia.Play("sufo");
-        dpsuba.Play("tifo");
+        blocka.Play("block-fade-in");
+    }
+
+    IEnumerator typetxt(float delayTime, string showTextTitle, string showTextSub)
+    {
+        block.SetActive(true);
+        dpti.GetComponent<Text>().text = "";
+        dpsub.GetComponent<Text>().text = "";
+        block.SetActive(false);
+        for (int j = 0; j < showTextTitle.Length; j++)
+        {
+            dpti.GetComponent<Text>().text = dpti.GetComponent<Text>().text + showTextTitle[j].ToString();
+            yield return new WaitForSeconds(delayTime);
+        }
+        for (int j = 0; j < showTextSub.Length; j++)
+        {
+            dpsub.GetComponent<Text>().text = dpsub.GetComponent<Text>().text + showTextSub[j].ToString();
+            yield return new WaitForSeconds(delayTime);
+        }
     }
 
 }
