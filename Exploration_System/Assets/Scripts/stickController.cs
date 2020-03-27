17ecaf0f6c18944b097694f9e4f3cbf2e8e2bac6
DavH-RSES <rses.davidh@gmail.com>
2020-03-27 11:04:20 +0800

    Joystick Development V.0
    Done:
     - Joystick Structure
    To Do:
     - Stick Following Mouse
     - Stick Following Position Restriction

---
+++ Exploration_System/Assets/Scripts/stickController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stickController : MonoBehaviour
+{
+    Vector2 mouse_pos;
+    Vector3 g_mouse_pos;
+    public bool follow_mouse = false;
+    public Camera g_cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouse_pos = Input.mousePosition;
+        g_mouse_pos = g_cam.ScreenToWorldPoint(mouse_pos);
+        if (follow_mouse) transform.position = g_mouse_pos;
+    }
+
+    public void followMouse()
+    {
+        follow_mouse = true;
+    }
+
+    public void unfollowMouse()
+    {
+        follow_mouse = false;
+    }
+}
